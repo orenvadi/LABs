e144077d2407d8420c350c0e9b514170ebf80761
hydroxygenium <orenvady@gmail.com>
2023-03-20 23:55:34 +0600

    initial commit

---
+++ methods/priority_func.py
@@ -0,0 +1,30 @@
+from .wrappers_speed import speedometer
+
+
+@speedometer
+def priority_sort(nums: list[int]):
+    queue = []
+    for num in nums:
+        q_insert(queue, num)
+    nums = queue
+
+
+def q_insert(queue: list, num):
+    # if queue is empty
+    if len(queue) == 0:
+        # add the new num
+        queue.append(num)
+    else:
+        # traverse the queue to find the right place for new num
+        for x in range(0, len(queue)):
+            # if the of new num is greater
+            if num >= queue[x]:
+                # if we have traversed the complete queue
+                if x == (len(queue) - 1):
+                    # add new num at the end
+                    queue.insert(x + 1, num)
+                else:
+                    continue
+            else:
+                queue.insert(x, num)
+                return True
---
+++ methods/wrappers_speed.py
@@ -0,0 +1,11 @@
+import time
+
+
+def speedometer(f):
+    def wrapper(*args, **kwargs) -> float:
+        start = time.perf_counter()
+        f(*args, **kwargs)
+        end = time.perf_counter()
+        return end - start
+
+    return wrapper
